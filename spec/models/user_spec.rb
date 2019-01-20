1e89b82ff82b85892061d8928532835a85a83c7a
dsgoers <daniel.goers@cru.org>
2019-01-20 14:37:54 -0500

    Add some more specs

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe User do
+  it 'requires a name' do
+    expect(described_class.create.errors[:name]).to include("can't be blank")
+  end
+end
