65c14dbda9085bbf3c06e917eeca95824932e9c6
dsgoers <daniel.goers@cru.org>
2019-01-20 15:19:33 -0500

    Add stores controller

---
+++ app/controllers/stores_controller.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class StoresController < ApplicationController
+  def index
+    load_stores
+
+    render jsonapi: @stores
+  end
+
+  def show; end
+
+  def create; end
+
+  def update; end
+
+  def destroy; end
+
+  private
+
+  def load_stores
+    @stores = Store.all
+  end
+end
