61300c1bf0f6adb7147bae0644495a14b51a0fa9
dsgoers <daniel.goers@cru.org>
2019-04-13 16:46:56 -0400

    Fix lint error

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -30,17 +30,21 @@ class GraphqlController < ApplicationController
   def ensure_hash(ambiguous_param)
     case ambiguous_param
     when String
-      if ambiguous_param.present?
-        ensure_hash(JSON.parse(ambiguous_param))
-      else
-        {}
-      end
+      handle_string(ambiguous_param)
     when Hash, ActionController::Parameters
       ambiguous_param
     when nil
       {}
     else
       raise ArgumentError, "Unexpected parameter: #{ambiguous_param}"
+    end
+  end
+
+  def handle_string(ambiguous_param)
+    if ambiguous_param.present?
+      ensure_hash(JSON.parse(ambiguous_param))
+    else
+      {}
     end
   end
 
